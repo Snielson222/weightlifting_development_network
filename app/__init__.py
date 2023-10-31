b052480bfd4d535e72c06ca3e731202ce43fd8fe
Steven Nielson <snielson222@gmail.com>
2023-10-31 14:11:33 -0400

    added aws support and routes

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,8 @@ from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .seeds import seed_commands
 from .config import Config
+from .api.exercise_routes import exercise_routes
+from .api.workout_routes import workout_routes
 
 app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
 
@@ -28,6 +30,8 @@ app.cli.add_command(seed_commands)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
+app.register_blueprint(exercise_routes, url_prefix='/api/exercise')
+app.register_blueprint(workout_routes, url_prefix='/api/workout')
 db.init_app(app)
 Migrate(app, db)
 
---
+++ app/forms/exercise_form.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms import SubmitField, StringField, IntegerField, TextAreaField, SelectField
+from ..api.aws_helpers import ALLOWED_EXTENSIONS
+from wtforms.validators import DataRequired
+
+class ExerciseForm(FlaskForm):
+    name = StringField("Name of Exercise", validators=[DataRequired()])
+    type = SelectField("type of Exercise", choices=["Upper Body", "Lower Body", "Warm Up"])
+    description = TextAreaField("Description of Exercise", validators=[DataRequired()])
+    image = FileField("Image File", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
+    submit = SubmitField("Create Post")
