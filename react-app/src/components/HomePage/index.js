92ed84c56f02a08cd71f6a216d35f552a43c8662
Steven Nielson <snielson222@gmail.com>
2023-11-06 10:59:55 -0500

    added all of the featured exercises and recent contributions to the home page

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useDispatch, useSelector} from "react-redux";
+import { useHistory, NavLink} from "react-router-dom/cjs/react-router-dom.min";
 import { thunkSearchExercises, thunkGetAllExercises } from "../../store/exercise";
 
 export default function HomePage() {
@@ -9,6 +9,23 @@ export default function HomePage() {
     const { push } = useHistory()
     const [search, setSearch] = useState("")
     const [errors, setErrors] = useState('')
+
+    const allExercises = useSelector((state) => state.exercises)
+    const featuredExercises = []
+    
+    for (let i = 0; i < 4; i++) {
+        featuredExercises.push(Object.values(allExercises)[i])
+    }
+    function sortDate(a, b) {
+        if (a.createdAt > b.createdAt) {
+            return -1;
+        } else if (a.createdAt < b.createdAt) {
+            return 1;
+        }
+            return 0;
+    }
+    const sortedByDateExercises = Object.values(allExercises).sort(sortDate)
+    console.log("🚀 ~ file: index.js:28 ~ HomePage ~ sortedByDateExercises:", sortedByDateExercises)
 
     useEffect(() => {
         dispatch(thunkGetAllExercises())
@@ -44,17 +61,57 @@ export default function HomePage() {
         </div>
             <h4>Featured Exercises</h4>
         <div id="featuredExercises">
-            <div id="featureContainer"></div>
-            <div id="featureContainer"></div>
-            <div id="featureContainer"></div>
-            <div id="featureContainer"></div>
+            <div id="featureContainer">
+                <NavLink className="featured" to={`exercise/${featuredExercises[0]?.id}`}>
+                    <h3>{featuredExercises[0]?.name}</h3>
+                    <p>{featuredExercises[0]?.description}</p>
+                </NavLink>
+            </div>
+            <div id="featureContainer">
+            <NavLink className="featured" to={`exercise/${featuredExercises[1]?.id}`}>
+                    <h3>{featuredExercises[1]?.name}</h3>
+                    <p>{featuredExercises[1]?.description}</p>
+                </NavLink>
+            </div>
+            <div id="featureContainer">
+            <NavLink className="featured" to={`exercise/${featuredExercises[2]?.id}`}>
+                    <h3>{featuredExercises[2]?.name}</h3>
+                    <p>{featuredExercises[2]?.description}</p>
+                </NavLink>
+            </div>
+            <div id="featureContainer">
+            <NavLink className="featured" to={`exercise/${featuredExercises[3]?.id}`}>
+                    <h3>{featuredExercises[3]?.name}</h3>
+                    <p>{featuredExercises[3]?.description}</p>
+                </NavLink>
+            </div>
         </div>
         <h4>Recent Contributions</h4>
         <div id="featuredExercises">
-            <div id="featureContainer"></div>
-            <div id="featureContainer"></div>
-            <div id="featureContainer"></div>
-            <div id="featureContainer"></div>
+            <div id="featureContainer">
+            <NavLink className="featured" to={`exercise/${sortedByDateExercises[0]?.id}`}>
+                    <h3>{sortedByDateExercises[0]?.name}</h3>
+                    <p>{sortedByDateExercises[0]?.description}</p>
+                </NavLink>
+            </div>
+            <div id="featureContainer">
+            <NavLink className="featured" to={`exercise/${sortedByDateExercises[1]?.id}`}>
+                    <h3>{sortedByDateExercises[1]?.name}</h3>
+                    <p>{sortedByDateExercises[1]?.description}</p>
+                </NavLink>
+            </div>
+            <div id="featureContainer">
+            <NavLink className="featured" to={`exercise/${sortedByDateExercises[2]?.id}`}>
+                    <h3>{sortedByDateExercises[2]?.name}</h3>
+                    <p>{sortedByDateExercises[2]?.description}</p>
+                </NavLink>
+            </div>
+            <div id="featureContainer">
+            <NavLink className="featured" to={`exercise/${sortedByDateExercises[3]?.id}`}>
+                    <h3>{sortedByDateExercises[3]?.name}</h3>
+                    <p>{sortedByDateExercises[3]?.description}</p>
+                </NavLink>
+            </div>
         </div>
         </div>)
 }
